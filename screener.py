ad7e5d04349800857cf84c9afe3e9ca89acd369f
sriramsrinivasan29 <43728410+sriramsrinivasan29@users.noreply.github.com>
2024-07-06 20:57:04 +0200

    Update screener.py

--- screener.py
+++ screener.py
@@ -14,13 +14,18 @@ from numpy import *
 
 from pandas_datareader import data as pdr
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 start = dt.datetime(2017, 12, 1)
 now = dt.datetime.now()
