55d9a39f841a1b1a40a1a0d06cca39b0421395c4
sriramsrinivasan29 <43728410+sriramsrinivasan29@users.noreply.github.com>
2024-07-06 20:58:47 +0200

    Update screener.py

--- screener.py
+++ screener.py
@@ -14,18 +14,7 @@ from numpy import *
 
 from pandas_datareader import data as pdr
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
 start = dt.datetime(2017, 12, 1)
 now = dt.datetime.now()
