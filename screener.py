594cb6e32281c8d148b242985b1b6c7ea96acc04
sriramsrinivasan29 <43728410+sriramsrinivasan29@users.noreply.github.com>
2025-02-21 17:02:46 +0100

    Update screener.py
    Adj Close error

--- screener.py
+++ screener.py
@@ -190,11 +190,12 @@ def stock_screener(index_ticker,end_date,indiaFlag,minerveni_flag):
     if indiaFlag :
         nifty = yf.download("^NSEI", "2010-1-1", end_date)
         stocklist = stocklist["Symbol"] + ".NS"
-        Std_Nifty_252 = nifty["Adj Close"].rolling(window=20).std()[-1]
+        #Std_Nifty_252 = nifty["Adj Close"].rolling(window=20).std()[-1]
+        Std_Nifty_252=1
     else:
         nifty = yf.download("^OMXS30", "2010-1-1", end_date)
         stocklist = stocklist["Symbol"]+ ".ST"
-        Std_Nifty_252 = 1
+        #Std_Nifty_252 = 1
 
     
     
