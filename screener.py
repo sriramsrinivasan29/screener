f245018c874e5769d674a2c61bdecc9952f946ab
sriramsrinivasan29 <43728410+sriramsrinivasan29@users.noreply.github.com>
2025-03-19 21:50:51 +0100

    Update screener.py

--- screener.py
+++ screener.py
@@ -15,11 +15,11 @@ from numpy import *
 from pandas_datareader import data as pdr
 from requests_ratelimiter import LimiterSession, RequestRate, Limiter, Duration
 
-history_rate = RequestRate(1, Duration.SECOND)
-limiter = Limiter(history_rate)
-session = LimiterSession(limiter=limiter)
+#history_rate = RequestRate(1, Duration.SECOND)
+#limiter = Limiter(history_rate)
+#session = LimiterSession(limiter=limiter)
 
-session.headers['User-agent'] = 'tickerpicker/1.0'
+#session.headers['User-agent'] = 'tickerpicker/1.0'
 
 
 start = dt.datetime(2017, 12, 1)
@@ -259,8 +259,8 @@ def stock_screener(index_ticker,end_date,indiaFlag,minerveni_flag):
             time.sleep(0.10)
             print("\npulling {} with index {}".format(ticker.split(".")[0], n))
             stock_industry = ""
-            stock = yf.download(ticker, "2017-5-2", end_date,session=session)
-            stockDetails = yf.Ticker(ticker,session=session)
+            stock = yf.download(ticker, "2017-5-2", end_date,proxy="PROXY_SERVER")
+            stockDetails = yf.Ticker(ticker,proxy="PROXY_SERVER")
             # get all stock info
             stockName=stockDetails.info['longName']
             if stock.size > 2000:
